e3a42097e0d2ff25da0750ef601807332cc1cdd3
yuliafedorov <65762233+yuliafedorov@users.noreply.github.com>
2020-06-18 14:21:22 +0300

    Added Entities to DAL

---
+++ AirMonitoring/DAL/Entities/Region.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Region
+    {
+        public int RegionID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int StationID { get; set; }
+        public Station Station { get; set; }
+    }
+}
---
+++ AirMonitoring/DAL/Entities/Station.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Station
+    {
+        public int StationID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Director { get; set; }
+
+        public IEnumerable<Region> Region { get; set; }
+    }
+}
