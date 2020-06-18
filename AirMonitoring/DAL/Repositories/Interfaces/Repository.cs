38d1d696ec317e5793131b3d65167ad0efce40c4
yuliafedorov <65762233+yuliafedorov@users.noreply.github.com>
2020-06-18 14:23:20 +0300

    Added Contracts of Repositories

---
+++ AirMonitoring/DAL/Repositories/Interfaces/Repository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        IEnumerable<T> Find(Func<T, Boolean> predicate, int pageNumber = 0, int pageSize = 10);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
---
+++ AirMonitoring/DAL/Repositories/Interfaces/StationRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+
+namespace DAL.Repositories.Interfaces
+{
+   
+    public interface IStationRepository
+       : IRepository<Station>
+    {
+    }
+}
